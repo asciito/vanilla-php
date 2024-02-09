524b3c7fdc8845bfb2816279a379cfb30c71f58a
Ayax Córdova <ayax.cordova@aydev.mx>
2024-02-09 15:59:54 -0600

    challenge-1: fix env variable name

--- 01-simple-blog-system/includes/config.php
+++ 01-simple-blog-system/includes/config.php
@@ -4,7 +4,7 @@ function db(): PDO
 {
     $host = $_ENV['DB_HOST'];
     $dbname = $_ENV['DB_NAME'];
-    $dbuser = $_ENV['BD_USER'];
+    $dbuser = $_ENV['DB_USER'];
     $dbpass = $_ENV['DB_PASS'] ?? '';
 
     return new PDO("mysql:host=$host;dbname=$dbname", $dbuser, $dbpass);
